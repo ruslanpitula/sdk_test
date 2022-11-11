7324627788eef6b8fa7310926a9120ca34e06228
Ruslan Pitula <ruslan.pitula@inoxoft.com>
2022-11-11 14:08:02 +0200

    2 targets

--- Package.swift
+++ Package.swift
@@ -10,19 +10,22 @@ let package = Package(
         .library(
             name: "TestSDK",
             targets: ["TestSDK"]),
+        .library(
+            name: "TestSDK-static",
+            targets: ["TestSDK-static"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-//        .target(
-//            name: "MyPackage",
-//            dependencies: []),
+
         .binaryTarget(name: "TestSDK",
         url: "https://download1325.mediafire.com/bw091f1e0srg/r8tdc4xmnzm5m14/TestSDK.zip",
                     checksum: "aa873df09fab2f1e087c3e3bfc8d6e912c1fad2a1283d3aafff05e90a8fefdd6"),
+        .binaryTarget(name: "TestSDK-static",
+        url: "https://download1476.mediafire.com/8oqgp5cfxc2g/ywvlz7oe2l808m5/TestSDK-static.zip",
+                    checksum: "40a485d61af12ab3e0b75dd5e6c86bcd7c706e2bf0c22ad722f0b5065c1ccf7a"),
     ]
 )
+
