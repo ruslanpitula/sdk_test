c8ebb093b8f547ca31494e4d957f050784e544f7
Ruslan Pitula <ruslan.pitula@inoxoft.com>
2022-11-11 18:31:10 +0200

    Updated hosting URLs

--- Package.swift
+++ Package.swift
@@ -6,7 +6,6 @@ import PackageDescription
 let package = Package(
     name: "Package-multiple-test",
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "TestSDK2",
             targets: ["TestSDK"]),
@@ -14,17 +13,14 @@ let package = Package(
             name: "TestSDK-static",
             targets: ["TestSDK-static"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
+    dependencies: [],
     targets: [
 
         .binaryTarget(name: "TestSDK",
-        url: "https://download1325.mediafire.com/bw091f1e0srg/r8tdc4xmnzm5m14/TestSDK.zip",
+        url: "https://download1325.mediafire.com/0js148utuobg/r8tdc4xmnzm5m14/TestSDK.zip",
                     checksum: "aa873df09fab2f1e087c3e3bfc8d6e912c1fad2a1283d3aafff05e90a8fefdd6"),
         .binaryTarget(name: "TestSDK-static",
-        url: "https://download1476.mediafire.com/8oqgp5cfxc2g/ywvlz7oe2l808m5/TestSDK-static.zip",
+        url: "https://download1476.mediafire.com/zmbz6hl3oszg/ywvlz7oe2l808m5/TestSDK-static.zip",
                     checksum: "40a485d61af12ab3e0b75dd5e6c86bcd7c706e2bf0c22ad722f0b5065c1ccf7a"),
     ]
 )
