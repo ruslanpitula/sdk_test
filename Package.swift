eeb52f486a313c8e9e2478d4a5c0e3d2b375eeee
Ruslan Pitula <ruslan.pitula@inoxoft.com>
2022-11-11 14:04:23 +0200

    comm

--- Package.swift
+++ Package.swift
@@ -8,8 +8,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "MyPackage",
-            targets: ["MyPackage"]),
+            name: "TestSDK",
+            targets: ["TestSDK"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -18,11 +18,11 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "MyPackage",
-            dependencies: []),
-        .testTarget(
-            name: "MyPackageTests",
-            dependencies: ["MyPackage"]),
+//        .target(
+//            name: "MyPackage",
+//            dependencies: []),
+        .binaryTarget(name: "TestSDK",
+        url: "https://download1325.mediafire.com/bw091f1e0srg/r8tdc4xmnzm5m14/TestSDK.zip",
+                    checksum: "aa873df09fab2f1e087c3e3bfc8d6e912c1fad2a1283d3aafff05e90a8fefdd6"),
     ]
 )
